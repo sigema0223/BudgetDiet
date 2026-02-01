d081b3ccbf2686215fd91b6837ed9d87f7d90151
sigema0223 <sigema0223@gmail.com>
2026-02-01 06:15:32 +0000

    rollback

--- vite.config.ts
+++ vite.config.ts
@@ -15,15 +15,5 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    minify: "esbuild",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 });
 
